ddbb5e571ef3a99a092590a5e71066e11feeb928
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2023-11-09 18:28:46 +0500

    Committing home template and url

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = {"cars": [
+        {"name": "Nexia3", "brand": "Chevrolet", "company": "GMmotors", "cost": 17000},
+        {"name": "Matiz", "brand": "Chevrolet", "company": "GMmotors", "cost": 3000},
+        {"name": "Mustang", "brand": "Ford", "company": "Ford", "cost": 59000},
+        {"name": "Supra", "brand": "Toyota", "company": "Toyota Motor Corporation", "cost": 57000}
+    ]}
+    return render(request, "home.html", context)
